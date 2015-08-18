1e59a2d605153e3c5e50a52070a034c9cd0afaad
Jeffrey Wan <Jwan622@gmail.com>
2015-08-18 17:33:21 +0100

    adding some handles and a user model

--- web.rb
+++ web.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'data_mapper'
+require 'bcrypt'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
 
@@ -14,7 +15,33 @@ class Item
   property :created, DateTime
 end
 
+class User
+  include DataMapper::Resource
+  property :id, Serial
+  property :username, Text, :require => true
+  property :password, Text, :require => true
+  property :created_at, DateTime
+end
+
 DataMapper.finalize.auto_upgrade!
+
+enable :sessions
+userTable = {}
+
+helpers do
+  def login?
+    if session[:username].nil?
+      return false
+    else
+      return true
+    end
+  end
+
+  def username
+    return session[:username]
+  end
+end
+
 
 get '/' do
   @items = Item.all(:order => :created.desc)
@@ -54,4 +81,31 @@ delete '/delete/:id' do
   else
     redirect '/'
   end
+end
+
+get "/signup" do
+  erb :signup
+end
+
+post "/signup" do
+  password_salt = BCrypt::Engine.generate_salt
+  password_hash = BCrypt::Engine.hash_secret(params[:password], password_salt)
+
+  User.create(:username => params[:username], :password => password_hash, :salt => password_salt)
+  session[:username] = params[:username]
+  redirect "/"
+end
+
+post "/login" do
+  user = User.find(:username => params[:username])
+  if user && user[:password] == BCrypt::Engine.hash_secret(params[:password], user[:salt])
+    session[:username] = params[:username]
+    redirect "/"
+  end
+  haml :error
+end
+
+get "/logout" do
+  session[:username] = nil
+  redirect "/"
 end
